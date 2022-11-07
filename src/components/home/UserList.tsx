4565386cb2ee2223291a36337752cce75a950b0f
Willian Lemann <willianleman@hotmail.com>
2022-11-06 21:17:19 -0300

    feat: user list card

--- src/components/home/UserList.tsx
+++ src/components/home/UserList.tsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
-import { differenceInYears, toDate } from "date-fns";
+import { differenceInYears } from "date-fns";
+import { Timestamp } from "firebase/firestore";
 
 import { getUsersService } from "@services/user/getUsersService";
 import { User } from "@models/user";
 import { addErrorNotification } from "@components/shared/alert";
+import { HeartIcon as OutlinedHeartIcon } from "@heroicons/react/24/outline";
+import { HeartIcon as FilledHeartIcon } from "@heroicons/react/24/solid";
 
 import Image from "next/image";
-import { Timestamp } from "firebase/firestore";
 
 export const UserList = () => {
   const [users, setUsers] = useState<User[]>([]);
@@ -33,30 +35,55 @@ export const UserList = () => {
   }, []);
 
   return (
-    <section className="flex-1">
-      <ul className="">
+    <section className="flex-1 p-4">
+      <ul className="md:grid md:grid-cols-2 gap-4">
         {users.map((user) => (
-          <li key={user.id} className="">
-            <div className="h-[200px] object-cover relative rounded-md">
-              <Image
-                className="rounded-md"
-                src={user.avatar as string}
-                alt="user avatar"
-                fill
-              />
-            </div>
+          <>
+            <li key={user.id}>
+              <div className="h-[200px] object-cover relative rounded-md">
+                <FilledHeartIcon  className="h-8 w-8 z-20 absolute right-2 top-2 cursor-pointer text-primary" />
 
-            <div>
-              <div>
-                <strong className="m-0">{user.name},</strong>
-                <span>{renderUserAgeLabel(user.birthday as Timestamp)}</span>
+                <Image
+                  className="rounded-md"
+                  src={user.avatar as string}
+                  alt="user avatar"
+                  fill
+                />
               </div>
 
-              <p className="m-0 text-sm leading-3 text-zinc-400">
-                10 kilometers away
-              </p>
-            </div>
-          </li>
+              <div>
+                <div>
+                  <strong className="m-0">{user.name},</strong>
+                  <span>{renderUserAgeLabel(user.birthday as Timestamp)}</span>
+                </div>
+
+                <p className="m-0 text-sm leading-3 text-zinc-400">
+                  10 kilometers away
+                </p>
+              </div>
+            </li>
+            <li key={user.id} className="">
+              <div className="h-[200px] object-cover relative rounded-md">
+                <Image
+                  className="rounded-md"
+                  src={user.avatar as string}
+                  alt="user avatar"
+                  fill
+                />
+              </div>
+
+              <div>
+                <div>
+                  <strong className="m-0">{user.name},</strong>
+                  <span>{renderUserAgeLabel(user.birthday as Timestamp)}</span>
+                </div>
+
+                <p className="m-0 text-sm leading-3 text-zinc-400">
+                  10 kilometers away
+                </p>
+              </div>
+            </li>
+          </>
         ))}
       </ul>
     </section>
