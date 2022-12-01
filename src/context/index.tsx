a5bc61a3980f26bc89b8c4eb78f0477eb13c51a5
Willian Lemann <willianleman@hotmail.com>
2022-12-01 19:46:15 -0300

    chore: env.example

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,13 +1,11 @@
 import { ReactNode } from "react";
 import { AuthProvider } from "./auth";
 import { ProviderComposer } from "./ProviderComposer";
-import { UsersProvider } from "./users";
+
 interface ProviderProps {
   children: ReactNode;
 }
 
 export const Provider = ({ children }: ProviderProps) => (
-  <ProviderComposer with={[AuthProvider, UsersProvider]}>
-    {children}
-  </ProviderComposer>
+  <ProviderComposer with={[AuthProvider]}>{children}</ProviderComposer>
 );
--- src/context/users/useUsers.tsx
+++ src/context/users/useUsers.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import { addErrorNotification } from "@components/shared/alert";
+
 import { User } from "@models/user";
 import { getUsersService } from "@services/user/getUsersService";
 
