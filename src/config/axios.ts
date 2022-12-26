624463c3eceb4af0e554e4c35c987bdf83f46bd9
Willian Lemann <willianleman@hotmail.com>
2022-12-26 15:40:05 -0300

    fix: app url vercel

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,9 +1,6 @@
 import axios from "axios";
 
-const APP_URL =
-  process.env.VERCEL === "1"
-    ? process.env.VERCEL_URL
-    : process.env.NEXT_PUBLIC_APP_URL;
+const APP_URL = process.env.NEXT_PUBLIC_APP_URL;
 
 export const api = axios.create({
   baseURL: `${APP_URL}/api`,
