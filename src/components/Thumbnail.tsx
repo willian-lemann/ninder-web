ab4c0476c47c4751af1e4e45f188afb222dc6436
Willian Lemann <willianleman@hotmail.com>
2023-01-03 14:31:09 -0300

    fix: placeholder image in usercard

--- src/components/Thumbnail.tsx
+++ src/components/Thumbnail.tsx
@@ -1,18 +1,22 @@
 import Image from "next/image";
+import { useState } from "react";
 
 interface ThumbnailProps {
   image: string | null;
 }
 
 export const Thumbnail = ({ image }: ThumbnailProps) => {
+  const [source, setSource] = useState(image as string);
+
   return (
     <div className="w-full h-full relative rounded-md">
       {image ? (
         <Image
           className="rounded-md object-cover"
-          src={image as string}
+          src={source}
           alt="user avatar"
           fill
+          onError={() => setSource("/icons/avatar.svg")}
         />
       ) : (
         <div className="flex items-center justify-center bg-zinc-200 rounded-md h-full">
