0420bcd9569c6177998ffa546e4ea8e5b2504802
Willian Lemann <willianleman@hotmail.com>
2023-03-14 20:52:18 -0300

    feat: add bottom sheet mobile user list

--- src/components/home/UserList/UserListBottomSheet.tsx
+++ src/components/home/UserList/UserListBottomSheet.tsx
@@ -1,7 +1,10 @@
+import { useUsers } from "@context/users";
+import Image from "next/image";
 import { useRef } from "react";
 import { BottomSheet, BottomSheetRef } from "react-spring-bottom-sheet";
 
 export function UserListBottomSheet() {
+  const { users } = useUsers();
   const sheetRef = useRef<BottomSheetRef>(null);
 
   return (
@@ -12,9 +15,31 @@ export function UserListBottomSheet() {
       className="md:hidden"
       expandOnContentDrag
       header={<header>More than 100 users near</header>}
-      snapPoints={({ maxHeight }) => [300, 600]}
+      snapPoints={() => [300, 600]}
     >
-      <div className="h-full p-4">aspdoksd asdoksad</div>
+      <div className="h-full p-4">
+        <ul className="grid grid-cols-3 place-items-center">
+          {users.map((user) => {
+            console.log(user.avatar);
+            return (
+              <li key={user.id} className=" flex flex-col items-center gap-2">
+                <div className="relative h-20 w-20">
+                  <Image
+                    src={user.avatar ?? "/icons/avatar.svg"}
+                    alt="user avatar"
+                    fill
+                    className="object-cover"
+                  />
+                </div>
+
+                <div>
+                  <h1>{user.name}</h1>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </BottomSheet>
   );
 }
